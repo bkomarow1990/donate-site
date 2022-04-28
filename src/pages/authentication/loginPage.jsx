47381054985dffc6696357bcfa79e7a9fec00413
VadimChorrny <chorrny228@gmail.com>
2022-04-28 10:54:28 +0300

    add logic for register and login with styles

--- src/pages/authentication/loginPage.jsx
+++ src/pages/authentication/loginPage.jsx
@@ -1,18 +1,109 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { login } from "../../services/authentication";
+import { AlertService } from "../../services/alertService";
+import { Form, Input, Button, Checkbox } from "antd";
+import { Link } from "react-router-dom";
+import { inputValidationErrors } from "../../constants/messages/inputValidationErrors";
+import "../../styles/authentication.css";
 
 export default function LoginPage() {
+    let history = useHistory();
+
+    const onFinish = (values) => {
+        login(values, history);
+    };
+
+    const onFinishFailed = () => {
+        AlertService.errorMessage(
+            "Log in is blocked!",
+            "First, correct all comments!"
+        );
+    };
+
     return (
-        <>
-            <form>
-                <span>Input your e-mail</span>
-                <input type="email" placeholder="yourmail@mail.com" />
-                <span>Input your password</span>
-                <input type="password" placeholder="123qwe1q2w3e" />
-                <button type="submit">Log In</button>
-                <a href="/registration" id="link">
-                    If you don't have an account, let's registration!
-                </a>
-            </form>
-        </>
+        <div className={"authBody"}>
+            <div className={"center"}>
+                <p className={"title"}>Logistman</p>
+                <p>Sign in and start managing your offers!</p>
+
+                <Form
+                    className={"form"}
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+                    initialValues={{ remember: true }}
+                    autoComplete="off"
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    scrollToFirstError
+                >
+                    <Form.Item
+                        name="email"
+                        className={"textForm"}
+                        rules={[
+                            {
+                                type: "email",
+                                message:
+                                    inputValidationErrors.NOT_VALID_EMAIL_MESSAGE,
+                            },
+                            {
+                                required: true,
+                                message:
+                                    inputValidationErrors.EMPTY_EMAIL_MESSAGE,
+                            },
+                        ]}
+                    >
+                        <Input placeholder="Email" />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="password"
+                        className={"passwordForm"}
+                        rules={[
+                            {
+                                type: "string",
+                                pattern: new RegExp(
+                                    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@$%^&*(){}:;<>,.?+_=|'~\\-])[A-Za-z0-9!@$%^&*(){}:;<>,.?+_=|'~\\-]{7,51}$"
+                                ),
+                                message:
+                                    inputValidationErrors.NOT_VALID_PASSWORD_MESSAGE,
+                            },
+                            {
+                                required: true,
+                                message:
+                                    inputValidationErrors.EMPTY_PASSWORD_MESSAGE,
+                            },
+                        ]}
+                    >
+                        <Input.Password
+                            className={"passwordInput"}
+                            placeholder="Password"
+                        />
+                    </Form.Item>
+
+                    <div className={"helperForm"}>
+                        <Checkbox className={"checkboxForm"}>
+                            Remember me
+                        </Checkbox>
+                        <Link to="/forgotPassword" id="forgot-password">Forgot password</Link>
+                    </div>
+
+                    <Form.Item className={"submitItem"}>
+                        <Button
+                            type="primary"
+                            htmlType="submit"
+                            className={"submitButton"}
+                        >
+                            Login
+                        </Button>
+                    </Form.Item>
+                </Form>
+
+                <div className={"linksDiv"}>
+                    <Link to="/home">Home</Link>
+                    <Link to="/registration">Registration</Link>
+                </div>
+            </div>
+        </div>
     );
 }
--- src/pages/authentication/registrationPage.jsx
+++ src/pages/authentication/registrationPage.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import { register } from "../../services/authentication";
 import { AlertService } from "../../services/alertService";
+import { Form, Input, Button } from "antd";
+import { Link } from "react-router-dom";
+import { inputValidationErrors } from "../../constants/messages/inputValidationErrors";
 import "../../styles/authentication.css";
 
 export default function RegistrationPage() {
@@ -19,21 +22,166 @@ export default function RegistrationPage() {
     };
 
     return (
-        <>
-            <form>
-                <span>Enter your name</span>
-                <input type="text" placeholder="Example: Vadym" />
-                <span>Enter your surname</span>
-                <input type="text" placeholder="Example: Chornyi" />
-                <span>Enter your e-mail</span>
-                <input type="email" placeholder="yourmail@mail.com" />
-                <span>Enter your password</span>
-                <input type="password" placeholder="123qwe1q2w3e" />
-                <button type="submit">Register</button>
-                <a href="/login" id="link">
-                    If you have an account, let's login!
-                </a>
-            </form>
-        </>
+        <div className="authBody">
+            <div className="center">
+                <p className="title">Registration</p>
+                <p>Sign up and start managing your tables!</p>
+
+                <Form
+                    className={"form"}
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+                    initialValues={{ remember: true }}
+                    autoComplete="off"
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    scrollToFirstError
+                >
+                    <Form.Item
+                        className={"textForm"}
+                        name="name"
+                        rules={[
+                            {
+                                type: "string",
+                                pattern: new RegExp("^[A-Z][a-z]+$"),
+                                message:
+                                    inputValidationErrors.NOT_VALID_NAME_MESSAGE,
+                            },
+                            {
+                                type: "string",
+                                min: 2,
+                                max: 50,
+                                message:
+                                    "The name must be between 1 and 50 letters!",
+                            },
+                            {
+                                required: true,
+                                message:
+                                    inputValidationErrors.EMPTY_NAME_MESSAGE,
+                            },
+                        ]}
+                    >
+                        <Input placeholder="Name" />
+                    </Form.Item>
+
+                    <Form.Item
+                        className={"textForm"}
+                        name="surname"
+                        rules={[
+                            {
+                                type: "string",
+                                pattern: new RegExp("^[A-Z][a-z]+$"),
+                                message:
+                                    inputValidationErrors.NOT_VALID_SURNAME_MESSAGE,
+                            },
+                            {
+                                type: "string",
+                                min: 2,
+                                max: 50,
+                                message:
+                                    "The name must be between 1 and 50 letters!",
+                            },
+                            {
+                                required: true,
+                                message:
+                                    inputValidationErrors.EMPTY_SURNAME_MESSAGE,
+                            },
+                        ]}
+                    >
+                        <Input placeholder="Surname" />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="email"
+                        className={"textForm"}
+                        rules={[
+                            {
+                                type: "email",
+                                message:
+                                    inputValidationErrors.NOT_VALID_EMAIL_MESSAGE,
+                            },
+                            {
+                                required: true,
+                                message:
+                                    inputValidationErrors.EMPTY_EMAIL_MESSAGE,
+                            },
+                        ]}
+                    >
+                        <Input placeholder="Email" />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="password"
+                        className={"passwordForm"}
+                        rules={[
+                            {
+                                type: "string",
+                                pattern: new RegExp(
+                                    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@$%^&*(){}:;<>,.?+_=|'~\\-])[A-Za-z0-9!@$%^&*(){}:;<>,.?+_=|'~\\-]{7,51}$"
+                                ),
+                                message:
+                                    inputValidationErrors.NOT_VALID_PASSWORD_MESSAGE,
+                            },
+                            {
+                                required: true,
+                                message:
+                                    inputValidationErrors.EMPTY_PASSWORD_MESSAGE,
+                            },
+                        ]}
+                    >
+                        <Input.Password
+                            className={"passwordInput"}
+                            placeholder="Password"
+                        />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="confirmedPassword"
+                        className={"passwordForm"}
+                        rules={[
+                            {
+                                required: true,
+                                message: inputValidationErrors.CONFIRM_PASSWORD,
+                            },
+                            ({ getFieldValue }) => ({
+                                validator(_, value) {
+                                    if (
+                                        !value ||
+                                        getFieldValue("password") === value
+                                    ) {
+                                        return Promise.resolve();
+                                    }
+                                    return Promise.reject(
+                                        new Error(
+                                            inputValidationErrors.PASSWORD_DONT_MATCH
+                                        )
+                                    );
+                                },
+                            }),
+                        ]}
+                    >
+                        <Input.Password
+                            className={"passwordInput"}
+                            placeholder="Confirm password"
+                        />
+                    </Form.Item>
+
+                    <Form.Item className={"submitItem"}>
+                        <Button
+                            type="primary"
+                            htmlType="submit"
+                            className={"submitButton"}
+                        >
+                            Register
+                        </Button>
+                    </Form.Item>
+                </Form>
+
+                <div className={"linksDiv"}>
+                    <Link to="/home">Home</Link>
+                    <Link to="/login">Login</Link>
+                </div>
+            </div>
+        </div>
     );
 }
