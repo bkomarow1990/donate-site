01ab8b487d614c5c06138b323be84483ad8526ec
VadimChorrny <chorrny228@gmail.com>
2022-04-29 09:25:22 +0300

    add output all tables

--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/header";
 import Table from "../components/table";
+import axios from "axios";
 import "../styles/main/mainPage.css";
 
 export default function MainPage() {
+    const [table, setTable] = useState([]);
+
+    const options = {
+        method: "GET",
+        url: "https://localhost:44386/api/Table",
+        params: {},
+        headers: {},
+    };
+
+    useEffect(() => {
+        axios
+            .request(options)
+            .then(function (response) {
+                setTable(response.data);
+                // console.log(table[1].name);
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+    }, []);
+
     return (
         <div className="container">
             <Header />
             <div className="wrapper">
                 <h1>That's main page</h1>
-                <Table
-                    name="Need armour"
-                    goal="New armour"
-                    image="https://collections.royalarmouries.org/media/emumedia/345/194/large_A5572_1.jpg"
-                    describe="something"
-                />
+                {table.map((elem) => (
+                    <Table
+                        name={elem.name}
+                        goal={elem.goal}
+                        image={elem.image}
+                        describe={elem.describe}
+                    />
+                ))}
             </div>
         </div>
     );
